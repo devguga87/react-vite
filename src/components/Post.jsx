d906162a96dcd9d52e4525b148fa81137fe7ea1e
Gustavo Seabra <devguga87@gmail.com>
2022-09-03 23:23:56 -0300

    adiciona componente de comentario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -23,6 +23,15 @@ const Post = () => {
 
         <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentario'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
