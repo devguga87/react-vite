680f08e77bb45ef86de1732adfe7e83089e06208
Gustavo Seabra <devguga87@gmail.com>
2022-09-04 00:31:30 -0300

    altera texto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=60" />
       <div className={styles.profile}>
         <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/55398608?v=4" />
-        <strong>Diego Fernandes</strong>
+        <strong>Gustavo Seabra</strong>
         <span>FrontEnd Developer</span>
       </div>
 
